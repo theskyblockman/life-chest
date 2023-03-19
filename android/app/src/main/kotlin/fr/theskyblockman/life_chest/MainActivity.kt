f22359ec8ff4d4ca6bab489d2a1377f1a5272bab
theskyblockman <theskyblockman@gmail.com>
2023-03-19 22:39:10 +0100

    Removed the ability to take screenshots from the app

--- android/app/src/main/kotlin/fr/theskyblockman/life_chest/MainActivity.kt
+++ android/app/src/main/kotlin/fr/theskyblockman/life_chest/MainActivity.kt
@@ -1,6 +1,12 @@
 package fr.theskyblockman.life_chest
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import android.view.WindowManager.LayoutParams
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        window.addFlags(LayoutParams.FLAG_SECURE);
+        super.onCreate(savedInstanceState)
+    }
 }
